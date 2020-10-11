2f5a89ad234382c255434602523b3140fdc8db02
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2020-10-11 12:44:42 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import TextField from "@material-ui/core/TextField";
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import { orange, green } from "@material-ui/core/colors";
+import "fontsource-roboto";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/ToolBar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B, #FF8E53)",
+    border: "0",
+    borderRadius: 15,
+    marginBottom: 30,
+    color: "white",
+    padding: "50 60px",
+  },
+});
+
+const theme = createMuiTheme({
+  // palette: {
+  //   primary: {
+  //     main: orange[100],
+  //   },
+  //   secondary: {
+  //     main: green[500],
+  //   },
+  // },
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Buttons</Button>;
+}
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checked}
+            checkedIcon={<SaveIcon />}
+            icon={<DeleteIcon />}
+            onChange={(event) => setChecked(event.target.checked)}
+            inputProps={{ "aria-label": "primary checkbox" }}
+          />
+        }
+        label="Testing Checkbox"
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="lg">
+        <div className="App">
+          <header className="App-header">
+            <AppBar color="primary">
+              <ToolBar>
+                <IconButton>
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">MUI Theme</Typography>
+                <Button>Login</Button>
+              </ToolBar>
+            </AppBar>
+            <Typography variant="h4">Welcome To MUI</Typography>
+            <Typography variant="subtitle1">
+              Lean how to use Material UI
+            </Typography>
+            <ButtonStyled />
+
+            <Grid container spacing={2} justify="center">
+              <Grid item xs>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+              <Grid item xs>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+              <Grid item xs>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+            </Grid>
+
+            <CheckboxExample />
+            <ButtonGroup variant="contained" color="primary">
+              <Button startIcon={<SaveIcon />} size="large">
+                Save
+              </Button>
+              <Button startIcon={<DeleteIcon />} size="large">
+                Discard
+              </Button>
+            </ButtonGroup>
+
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
